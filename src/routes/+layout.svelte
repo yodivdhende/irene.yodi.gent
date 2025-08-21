21d99f189068518cb6534802f1a08e20fa641067
Yodi <yodi.vandenhende@gmail.com>
2025-08-21 22:31:18 +0200

    adding title image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import "../reset.css";
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+  import  homeTitleImage from "$lib/assets/home-title.png"
+</script>
+<main>
+  <div class="background">
+    <img alt="Irene & Yodi title image" src={homeTitleImage} />
+  </div>
+  <div class="title-placeholder"></div>
+  <div class="images">
+    <img alt="test image" src="https://picsum.photos/300/400"/>
+    <img alt="test image" src="https://picsum.photos/300/400"/>
+    <img alt="test image" src="https://picsum.photos/300/400"/>
+    <img alt="test image" src="https://picsum.photos/300/400"/>
+  </div>
+</main>
+<style>
+  main{
+    display: flex;
+    flex-direction: column;
+  } 
+  .background {
+    position:sticky;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    overflow:hidden;
+    background-color: hsl(42,15,95)
+
+  }
+  .title-placeholder {
+    width: calc(100vw - 1px);
+    height: calc(100vh - 1px);
+    overflow: hidden;
+    border: 1px solid red;
+  }
+
+  .images {
+    display: flex;
+    height: 100vh;
+    background-color: aliceblue;
+  }
+</style>
