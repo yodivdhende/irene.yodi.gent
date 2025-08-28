1a0e55afa2a1dbae00d0d52352a8b445f00291c0
Yodi <yodi.vandenhende@gmail.com>
2025-08-28 22:23:25 +0200

    styling navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,27 @@
 </main>
 
 <style>
+	main {
+		width: 100%;
+		position:absolute;
+		bottom:0;
+		display: flex;
+		flex-direction: row;
+		background-color: hsl(95 50% 90%);
+	}
+	a {
+		display: block;
+		width: 100%;
+		text-decoration: none;
+		font-size: 2em;
+		font-family: 'Courier New', Courier, monospace;
+		font-weight: bolder;
+		text-align: center;
+		color: darkgreen;
+		padding: 1em;
+	}
+
+	main a:first-child{
+		border-right: 1px solid darkgreen;
+	}
 </style>
